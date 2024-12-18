f5de5214c3e50fff0125689f2f9567431b84c2f4
Seamus Jackson <63567335+Schweem@users.noreply.github.com>
2024-12-18 02:05:36 -0500

    latest data and header changes

--- src/frontEnd/src/components/Header.js
+++ src/frontEnd/src/components/Header.js
@@ -8,8 +8,8 @@ const Header = () => {
                 <div className="logo">Tampa Bay Twitter Red Tide Analytics Dashboard</div>
                 <p>
                     <ul className="bodyText">
-                        <strong>Objective:</strong> Our goal is to collect, analyze, and present publicly available twitter data to users in a digestible way. <br/>
-                        <strong>Source of Data:</strong> All of these tweets are collected from X due to them pertaining/mentioning Red Tide and/or the Tampa Bay area 5 Key counties.
+                        <strong className="item">Objective:</strong> Primary purpose of the dashboard is to provide regional stakeholders, researchers and managers with a tool that allows retrospective analysis of public discussions about red tide. It should help summarize most cited public concerns, level of awareness and education around the issue. It should not be used as a source of reliable information on the red tide itself.<br/>
+                        <strong className="item">Source of Data:</strong> All of these tweets are collected from X due to them pertaining/mentioning Red Tide and/or the Tampa Bay area 5 Key counties.
                         <p className="disclaimer">For ideal perfomance and layout, please use on a laptop or desktop PC.</p>
                     </ul>
                 </p>
